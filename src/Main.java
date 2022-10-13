8fe6f6bea0df193f85551d6d263883e120e7d84e
Mouhammad Azroun <mouhammadazroun@Mouhammads-MacBook-Air.local>
2022-10-13 03:03:33 +0200

    Added a code to the main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("The first persons name and age are: ");
+        Person p1 = new firstPerson("Youssef",25);
+        System.out.println(p1.toString());
+        System.out.println("The second persons name and age are:");
+        Person p2 = new secondPerson("Mohammad", 27);
+        System.out.println(p2.toString());
 
     }
 }
